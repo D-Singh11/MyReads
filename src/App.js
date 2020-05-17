55a2811dea72ddee4be81b3ca0702ac35282a57a
DS <sssscorejava@gmail.com>
2020-05-17 23:35:21 +0100

    feat: Apply routing
    Add Route component/tag from react-router-dom to SerachBooks component and add /search path to the path property. Use render prop of Route to
    show SearchBooks component. Similarly, include rest of the app inside Route with / route which is home route using path prop. Using render prop of Route to show the book shelfs when route is exactly matched with home route

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css'
 import SearchBooks from './components/SearchBooks'
 import BookShelf from './components/BookShelf'
 import { Link } from 'react-router-dom'
+import { Route } from 'react-router-dom'
 
 
 class BooksApp extends React.Component {
@@ -19,26 +20,29 @@ class BooksApp extends React.Component {
   render() {
     return (
       <div className="app">
-        <SearchBooks />
+        <Route path="/search" render={() => (
+          <SearchBooks />
+        )} />
 
-        <div className="list-books">
-          <div className="list-books-title">
-            <h1>MyReads</h1>
-          </div>
-          <div className="list-books-content">
-            <div>
-              <BookShelf title={"Currently Reading"} />
-              <BookShelf title={"Want to Read"} />
-              <BookShelf title={"Read"} />
+        <Route exact path="/" render={() => (
+          <div className="list-books">
+            <div className="list-books-title">
+              <h1>MyReads</h1>
+            </div>
+            <div className="list-books-content">
+              <div>
+                <BookShelf title={"Currently Reading"} />
+                <BookShelf title={"Want to Read"} />
+                <BookShelf title={"Read"} />
+              </div>
+            </div>
+            <div className="open-search">
+              <Link to="/search">
+                <button>Add a book</button>
+              </Link>
             </div>
           </div>
-          <div className="open-search">
-            <Link to='/search'>
-              <button>Add a book</button>
-            </Link>
-          </div>
-        </div>
-
+        )} />
       </div>
     )
   }
