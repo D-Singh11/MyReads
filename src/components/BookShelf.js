3fd7da997e2127da351df74cfe47f858ed3522d2
DS <sssscorejava@gmail.com>
2020-05-17 22:21:49 +0100

    fix : Define types for incmoing using PropTypes to the BookShelf component and accept bookshelf title as props

--- src/components/BookShelf.js
+++ src/components/BookShelf.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react'
+import BookList from './BookList'
+import PropTypes from 'prop-types'
 
 class BookShelf extends Component {
+    static propTypes = {
+        title: PropTypes.string.isRequired
+    }
+
     render() {
         return (
-            <div>
-                
+            <div className="bookshelf">
+                <h2 className="bookshelf-title">{this.props.title}</h2>
+                <div className="bookshelf-books">
+                    <BookList />
+                </div>
             </div>
         )
     }
