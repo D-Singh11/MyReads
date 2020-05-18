46f41d0df6e034b161de0f6a1344016c83c3a6e3
DS <sssscorejava@gmail.com>
2020-05-18 14:56:02 +0100

    refctor: Update Book and BookList components to use dynamic data later on

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -3,14 +3,12 @@ import Book from './Book'
 
 class BookList extends Component {
     render() {
+        const list = this.props.books.map(book => {
+            return (<li key={book.id}><Book book={book} /></li>)
+        });
         return (
             <ol className="books-grid">
-                <li>
-                    <Book />
-                </li>
-                <li>
-                    <Book />
-                </li>
+                {list}
             </ol>
         )
     }
