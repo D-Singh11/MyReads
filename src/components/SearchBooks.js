ab6e7284a628776782406e3c0f430903af06ad73
DS <sssscorejava@gmail.com>
2020-05-18 15:32:13 +0100

    feat : Add live book serach
    Added onClick EventListner to Search input of SerachBooks component.Create handleChange Event which calls the API by using the value from serach input.
    Add local state to SerachBooks component and update it when serach return response.
    Display serach results by using BookList Compoenet which accept books array as prop.

--- src/components/SearchBooks.js
+++ src/components/SearchBooks.js
@@ -1,14 +1,33 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import * as BooksAPI from '../BooksAPI'
+import BookList from './BookList';
 
 class SearchBooks extends Component {
-    render() {
-        return (
-            <div className="search-books">
-            <div className="search-books-bar">
-              <Link to="/" className="close-search">Close</Link>
-              <div className="search-books-input-wrapper">
-                {/*
+
+  state = {
+    books: []
+  }
+
+  handleChange = (event) => {
+    const serachedBooks = event.target.value !== '' ?
+      (BooksAPI.search(event.target.value).then(books => {
+        this.setState({
+          books
+        });
+        console.log(this.state);
+      })) : this.setState({
+        books :[]
+      });
+  }
+
+  render() {
+    return (
+      <div className="search-books">
+        <div className="search-books-bar">
+          <Link to="/" className="close-search">Close</Link>
+          <div className="search-books-input-wrapper">
+            {/*
                   NOTES: The search from BooksAPI is limited to a particular set of search terms.
                   You can find these search terms here:
                   https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
@@ -16,16 +35,17 @@ class SearchBooks extends Component {
                   However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
                   you don't find a specific author or title. Every search is limited by search terms.
                 */}
-                <input type="text" placeholder="Search by title or author"/>
+            <input type="text" placeholder="Search by title or author" onChange={this.handleChange} />
 
-              </div>
-            </div>
-            <div className="search-books-results">
-              <ol className="books-grid"></ol>
-            </div>
           </div>
-        )
-    }
+        </div>
+        <div className="search-books-results">
+          <ol className="books-grid"></ol>
+          { this.state.books.length > 0 && <BookList books={this.state.books}/>}
+        </div>
+      </div>
+    )
+  }
 }
 
 export default SearchBooks;
