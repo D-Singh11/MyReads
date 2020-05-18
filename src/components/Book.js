d7399c8ce890b346633a14fcb1d6c949d3ceecda
DS <sssscorejava@gmail.com>
2020-05-18 22:30:55 +0100

    feat: Get user selection when its shelf category change
    Only user selection is read when user click on the category of shelf. Adding the book to the actual shelf will be implemented next.
    Guard the author and thumbnails display if book has no author or thumbnail
    Trim search input to remove spaces

--- src/components/Book.js
+++ src/components/Book.js
@@ -2,14 +2,18 @@ import React, { Component } from 'react'
 
 class Book extends Component {
 
+    handleSelect= (event) => {
+        const shelf = event.target.value;
+        console.log(shelf);
+    }
     render() {
         const {book} = this.props;
         return (
             <div className="book">
                 <div className="book-top">
-                    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
+                    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks !== undefined && book.imageLinks.thumbnail})` }}></div>
                     <div className="book-shelf-changer">
-                        <select>
+                        <select onChange={this.handleSelect} defaultValue={ book.hasOwnProperty('shelf') ? book.shelf : 'none'}>
                             <option value="move" disabled>Move to...</option>
                             <option value="currentlyReading">Currently Reading</option>
                             <option value="wantToRead">Want to Read</option>
@@ -19,7 +23,7 @@ class Book extends Component {
                     </div>
                 </div>
                 <div className="book-title">{book.title}</div>
-                {book.authors != undefined &&
+                {book.authors !== undefined &&
                     <div className="book-authors">
                         {book.authors.map(author => <p key={author} >{author}</p>)}
                     </div>}
